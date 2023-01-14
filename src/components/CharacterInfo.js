cec653cf3714115a2456eb8219586aafcff9845a
Austin Aluzzi <austinaluzzi@gmail.com>
2023-01-14 01:32:53 -0800

    Display sprite in character info

--- src/components/CharacterInfo.js
+++ src/components/CharacterInfo.js
@@ -1,10 +1,23 @@
 function CharacterInfo(props) {
-    return (
-        <div className="character-info">
-            <div>{props.character.found ? "✔️" : "❓"}</div>
-            <div className="character-name"><b>{props.character.name}</b></div>
-        </div>
-    );
+    if (props.character.found) {
+        return (
+            <div className="character-info">
+                <div className="sprite found" style={{backgroundImage: `url(${getSpriteUrl(props.character)})`}}></div>
+                <div className="character-name found">{props.character.name}</div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="character-info">
+                <div className="sprite" style={{backgroundImage: `url(${getSpriteUrl(props.character)})`}}></div>
+                <div className="character-name">{props.character.name}</div>
+            </div>
+        );
+    }
+}
+
+function getSpriteUrl(pokemon) {
+    return `https://img.pokemondb.net/sprites/black-white/normal/${pokemon.name.toLowerCase()}.png`
 }
 
 export default CharacterInfo;
