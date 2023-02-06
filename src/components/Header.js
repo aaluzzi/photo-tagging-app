f144f9679871b28ca08e573f51656d7b73fc6cc2
Austin Aluzzi <austinaluzzi@gmail.com>
2023-02-05 23:10:02 -0800

    Optimize header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,27 +7,23 @@ function Header(props) {
     if (props.status === "playing") {
         return (
             <div className="header">
-                <div className="content">
-                    <div id="logo">
-                        <img className="pokeball" src={Pokeball} alt="Pokeball"/>
-                        <div className="logo-text">Find That Pokemon</div>
-                    </div>
-                    <CharacterDisplay characters={props.characters} />
-                    <Stopwatch />    
+                <div id="logo">
+                    <img className="pokeball" src={Pokeball} alt="Pokeball" />
+                    <div className="logo-text">Find That Pokemon</div>
                 </div>
+                <CharacterDisplay characters={props.characters} />
+                <Stopwatch />
             </div>
-        ); 
+        );
     } else {
         return (
             <div className="header">
-                <div className="content">
-                    <div id="logo">
-                        <img className="pokeball" src={Pokeball} alt="Pokeball"/>
-                        <div className="logo-text">Find That Pokemon</div>
-                    </div>
+                <div id="logo">
+                    <img className="pokeball" src={Pokeball} alt="Pokeball" />
+                    <div className="logo-text">Find That Pokemon</div>
                 </div>
             </div>
-        ); 
+        );
     }
 }
 export default Header;
